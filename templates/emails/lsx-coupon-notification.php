327cac074ddad18c02d8ab8ffb0c0cc30b15cf01
James La Guma <james@lsdev.biz>
2019-12-02 10:21:44 +0200

    another travis fix

--- templates/emails/lsx-coupon-notification.php
+++ templates/emails/lsx-coupon-notification.php
@@ -22,7 +22,7 @@ do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 <p><?php printf( esc_html__( 'Hi %s,', 'woocommerce' ), esc_html( $order->get_billing_first_name() ) ); ?></p>
 <p><?php esc_html_e( 'The following note has been added to your order:', 'woocommerce' ); ?></p>
 
-<blockquote><?php echo wpautop( wptexturize( make_clickable( $customer_note ) ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></blockquote>
+<blockquote><?php echo wp_kses_post( wpautop( wptexturize( make_clickable( $customer_note ) ) ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></blockquote>
 
 <p><?php esc_html_e( 'As a reminder, here are your order details:', 'woocommerce' ); ?></p>
 
